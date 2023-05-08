e4c6397d69d30aba17b03dd486ddf9b8ef6ac7b6
ayoubbr <ayoub1benmansour@gmail.com>
2023-05-08 11:09:45 +0100

    Creating API to get cart details

--- src/main/java/com/spring/ecommerce/dao/CartDao.java
+++ src/main/java/com/spring/ecommerce/dao/CartDao.java
@@ -1,9 +1,13 @@
 package com.spring.ecommerce.dao;
 
 import com.spring.ecommerce.entity.Cart;
+import com.spring.ecommerce.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartDao extends CrudRepository<Cart, Integer> {
+    List<Cart> findByUser(User user);
 }
--- src/main/java/com/spring/ecommerce/service/CartService.java
+++ src/main/java/com/spring/ecommerce/service/CartService.java
@@ -10,6 +10,9 @@ import com.spring.ecommerce.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class CartService {
@@ -33,5 +36,11 @@ public class CartService {
             return cartDao.save(cart);
         }
         return null;
+    }
+
+    public List<Cart> getCartDetails() {
+        String username = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(username).get();
+        return cartDao.findByUser(user);
     }
 }
