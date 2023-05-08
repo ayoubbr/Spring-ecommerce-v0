f50dac9986624df89788154583fd54fa5d7d1516
ayoubbr <ayoub1benmansour@gmail.com>
2023-05-08 12:28:01 +0100

    Adding API to checkout the cart

--- src/main/java/com/spring/ecommerce/service/ProductService.java
+++ src/main/java/com/spring/ecommerce/service/ProductService.java
@@ -1,7 +1,12 @@
 package com.spring.ecommerce.service;
 
+import com.spring.ecommerce.configuration.JwtRequestFilter;
+import com.spring.ecommerce.dao.CartDao;
 import com.spring.ecommerce.dao.ProductDao;
+import com.spring.ecommerce.dao.UserDao;
+import com.spring.ecommerce.entity.Cart;
 import com.spring.ecommerce.entity.Product;
+import com.spring.ecommerce.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +14,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private CartDao cartDao;
 
     public Product addNewProduct(Product product) {
         return productDao.save(product);
@@ -39,7 +49,7 @@ public class ProductService {
     }
 
     public List<Product> getProductDetails(boolean isSingleProductCheckout, Integer productId) {
-        if (isSingleProductCheckout) {
+        if (isSingleProductCheckout && productId != 0) {
             // buy a single product
             List<Product> productList = new ArrayList<>();
             Product product = productDao.findById(productId).get();
@@ -47,7 +57,10 @@ public class ProductService {
             return productList;
         } else {
             // checkout entire cart
+            String username = JwtRequestFilter.CURRENT_USER;
+            User user = userDao.findById(username).get();
+            List<Cart> carts = cartDao.findByUser(user);
+            return carts.stream().map(x -> x.getProduct()).collect(Collectors.toList());
         }
-        return new ArrayList<>();
     }
 }
